31d2eaaf58bffc9b88c7b61fc998f79530472a2e
Felipe Cruz <felipephplow@gmail.com>
2022-03-04 15:07:44 -0300

    a

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -43,7 +43,7 @@
                 <a href="{{ route('produtos') }}" class="list-group-item list-group-item-action">PRODUTOS E SERVIÇOS</a>
                 <a href="{{ route('bonus') }}" class="list-group-item list-group-item-action">BÔNUS</a>
                 <a href="#" class="list-group-item list-group-item-action">PREMIOS</a>
-                <a href="http://app.gmbrasilvd.com.br/" class="list-group-item list-group-item-action disabled">ESCRITÓRIO VIRTUAL</a>
+                <a href="http://app.gmbrasilvd.com.br/" class="list-group-item list-group-item-action">ESCRITÓRIO VIRTUAL</a>
                 <a class="list-group-item list-group-item-action disabled">CONTATO</a>
             </div>
         </div>
